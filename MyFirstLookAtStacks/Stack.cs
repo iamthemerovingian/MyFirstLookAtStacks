9877a799b2e62214c1ad6b9b0ad4d5a1cd778189
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-02-01 17:54:07 +0700

    Stack implementation using a LinkList

---
+++ MyFirstLookAtStacks/Stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstLookAtStacks
+{
+    public class Stack<T>: IEnumerable<T>
+    {
+        private LinkedList<T> _list = new LinkedList<T>();
+
+        public int Count
+        {
+            get
+            {
+                return _list.Count();
+            }
+        }
+        private T Peek()
+        {
+            IsStackEmpty();
+            return _list.First();
+        }
+
+        private void IsStackEmpty()
+        {
+            if (_list.Count == 0)
+            {
+                throw new Exception("Your Stack Is Empty");
+            }
+        }
+
+        private T Pop()
+        {
+            IsStackEmpty();
+
+            T topMostItem = _list.First();
+            _list.RemoveFirst();
+
+            return topMostItem;
+        }
+
+        private void Push(T item)
+        {
+            _list.AddFirst(item);
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_list).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)_list).GetEnumerator();
+        }
+    }
+
+
+}
