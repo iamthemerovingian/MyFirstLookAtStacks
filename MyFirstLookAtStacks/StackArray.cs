ffb7f6e1ceab14bdeb2e11a1bdb1c32dc1c9bcc6
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-02-01 18:21:26 +0700

    Stack Implementation usinng an Array. GetEnumerator must be completed.

--- MyFirstLookAtStacks/StackArray.cs
+++ MyFirstLookAtStacks/StackArray.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,70 @@ using System.Threading.Tasks;
 
 namespace MyFirstLookAtStacks
 {
-    class Stack
+    class StackArray<T> : IEnumerable<T>
     {
+        private int _size = 0;
+		private T[] _items = new T[0];
+
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+
+        public void Push(T item)
+        {
+            if (_size == _items.Length)
+            {
+                int newLength = _size == 0 ? 4 : _size * 2 ;
+
+				T[] temp = new T[newLength];
+                _items.CopyTo(temp, 0);
+
+                _items = temp;
+            }
+
+            _items[_size] = item;
+            _size++;
+        }
+
+		public T Peek()
+        {
+            IsStackEmpty();
+
+            return _items[_size - 1];
+        }
+
+		private void IsStackEmpty()
+        {
+            if (_size == 0)
+            {
+                throw new Exception("Stack Is empty");
+            }
+        }
+
+		public T Pop() 
+        {
+            IsStackEmpty();
+            _size--;
+            return _items[_size];
+        }
+
+        public void Clear()
+        {
+            _size = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_items).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)_items).GetEnumerator();
+        }
     }
 }
